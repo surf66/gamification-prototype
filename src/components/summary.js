cd31ce7cbd253074d66e6183f8de48c54d29fa0f
Tom Pennington <tompennington.92@gmail.com>
2017-11-29 10:16:03 +0000

    Add vehicles to dashboard on add to shortlist.

--- src/components/summary.js
+++ src/components/summary.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { completeTask } from '../actions/index';
+import VehicleCard from './vehicle-card';
+import './summary.css';
 
 function mapDispatchToProps(dispatch) {
   return ({
@@ -19,7 +21,15 @@ export class Summary extends React.Component {
   render() {
     return (
       <div>
-        <p>Summary</p>
+        <div className="profile">
+          <img className="profile-image" src="./images/james.png" alt="James" />
+          <p><strong>Welcome back James</strong></p>
+        </div>
+        {this.props.tasks.shortlistVehicle &&
+          <section>
+            <VehicleCard />
+          </section>
+        }
       </div>
     );
   }
